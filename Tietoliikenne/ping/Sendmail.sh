8ef08073e12105181d51782b1a1668201b2172bd
Atte Oksanen <37768995+Mixerboy24@users.noreply.github.com>
2019-03-20 08:51:57 +0200

    Create Sendmail.sh

---
+++ Tietoliikenne/ping/Sendmail.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+HOSTS="mixerboy24.dyndns.org"
+COUNT=4
+
+pingtest(){
+  for myHost in "$HOSTS"
+  do
+    ping -c "$COUNT" "$myHost" && return 1
+  done
+  return 0
+}
+
+if pingtest $HOSTS
+then
+  # 100% failed
+  echo "Server failed at $(date)" | mail -s "Webserver1 down" contact@mixerboy24tech.fi
+  echo "All hosts ($HOSTS) are down (ping failed) at $(date)"
+fi
